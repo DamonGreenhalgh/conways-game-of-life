291d0c5d3fce10acc79f12cd436550264e74ca1a
Damon Greenhalgh <damonligreenhalgh@gmail.com>
2021-07-06 01:33:36 +1200

    Added Speed Slider
    Added a slider that represents the delay of new iterations.

--- src/GUI.java
+++ src/GUI.java
@@ -7,28 +7,33 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class GUI{
     private GameBoard board;
     private int size;    
     private int numIterations;
     private JLabel lblNumIterations;
+    private int delay;
+    private Timer timer;
+    private boolean timerState;
 
     public GUI() {
-        this.board = new GameBoard(25);
-        size = board.getSize();
+        this.board = new GameBoard(20);
+        this.size = board.getSize();
+        this.delay = 1000;    // one second delay
 
         // create frame and panels
         JFrame frame = new JFrame();  
-
         JPanel gridPanel = new JPanel(new GridLayout(size, size));
+        JPanel optionsPanel = new JPanel(new GridLayout(1, 5));
+        JPanel sliderPanel = new JPanel(new GridLayout(2, 1));
 
-        GridLayout optionsLayout = new GridLayout(1, 4);
-        optionsLayout.setHgap(5);
-        optionsLayout.setVgap(5);
-        JPanel optionsPanel = new JPanel(optionsLayout);
+        ((GridLayout)optionsPanel.getLayout()).setHgap(10);
+        ((GridLayout)optionsPanel.getLayout()).setVgap(10);
         
-        // set up grid panel
+        // set up grid components
         for(int i = 0; i < size; i++) {
             for(int j = 0; j < size; j++) {
                 // grid details
@@ -39,12 +44,12 @@ public class GUI{
             }
         }
 
-        // set up options panel
+        // set up options components
         // start/stop button, used to start/stop the simulation
+        timer = new Timer(delay, new MyStopStartListener());
+        timerState = true;
         JButton btnStartStop = new JButton("Start");
         btnStartStop.addActionListener(new AbstractAction(){
-            private Timer timer = new Timer(1000, new nextIteration());
-            private boolean timerState = true;
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(timerState) {
@@ -62,13 +67,36 @@ public class GUI{
         // clear button, used to clear the board and reset the number of iterations
         JButton btnClear = new JButton("Clear");
         btnClear.addActionListener(new AbstractAction(){
+            @Override
             public void actionPerformed(ActionEvent e) {
                 board.clear();
                 update();
+                timer.stop();
+                timerState = true;
+                btnStartStop.setText("Start");
                 numIterations = 0;
                 lblNumIterations.setText("Number of Iterations: " + numIterations);
             }
         });
+
+        // speed slider, used to change the delay of each new iteration
+        JLabel lblSpeed = new JLabel("Speed: 1x");
+        lblSpeed.setHorizontalAlignment(0);
+
+        JSlider sdrSpeed = new JSlider(1, 8);
+        sdrSpeed.setPaintTicks(true);
+        sdrSpeed.setMinorTickSpacing(1);
+        sdrSpeed.addChangeListener(new ChangeListener(){
+            public void stateChanged(ChangeEvent e) {
+                delay = 250 * Math.abs(sdrSpeed.getValue() - 8);
+                timer.setDelay(Math.max(delay, 250));
+                lblSpeed.setText("Speed: " + 250 * sdrSpeed.getValue() / 1000.0 + "x");
+            }
+        });
+
+        // add slider components to slider panel
+        sliderPanel.add(lblSpeed);
+        sliderPanel.add(sdrSpeed);
 
         // number of iterations label, used to display the current number of iterations
         lblNumIterations = new JLabel("Number of Iterations: " + numIterations);
@@ -77,6 +105,7 @@ public class GUI{
         // add options components to optionPanel
         optionsPanel.add(btnStartStop);
         optionsPanel.add(btnClear);
+        optionsPanel.add(sliderPanel);
         optionsPanel.add(lblNumIterations);
 
         // add panels to frame
@@ -102,7 +131,7 @@ public class GUI{
         }
     }
 
-    private class nextIteration implements ActionListener {
+    private class MyStopStartListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             board.next();
@@ -123,7 +152,6 @@ public class GUI{
                 node.setState(false);
                 node.setBackground(Color.WHITE);
             }
-            board.display();
         }
     }
 }
